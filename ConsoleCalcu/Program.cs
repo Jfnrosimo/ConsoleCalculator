82b6009a68af4862bf029d40a366c69d2d7a2824
Jfnrosimo <j.rosimo098.com>
2023-02-12 14:39:10 +0800

    Feat: add simple calculator

---
+++ ConsoleCalcu/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("Simple Calculator");
+Console.WriteLine("Enter the first number:");
+double num1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Enter the first number:");
+double num2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("What operation would you like to perform? (+, -, *, /):");
+string operation = Console.ReadLine();
+
+double result;
+
+switch (operation)
+{
+    case "+":
+        result = num1 + num2;
+        Console.WriteLine("The result after addition is: " + result);
+        break;
+
+    case "-":
+        result = num1 - num2;
+        Console.WriteLine("The result after subtraction is: " + result);
+        break;
+
+    case "*":
+        result = num1 * num2;
+        Console.WriteLine("The result after multiplication is: " + result);
+        break;
+
+    case "/":
+        result = num1 / num2;
+        Console.WriteLine("The result after multiplication is: " + result);
+        break;
+}
+
